c7c1b4a48b373efe17d236aecce6c411ee90d17f
Savva Siry <savvasiry@gmail.com>
2023-12-27 13:26:27 +1100

    feature(Project): add base structure

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="header">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppHeader"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text text_h1">
+    Привет мир!
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage"
+}
+</script>
+
+<style lang="less" scoped>
+
+
+</style>
