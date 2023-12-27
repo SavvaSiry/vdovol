0124b83d23b6455008e40f5f96954fa754aa0090
Savva Siry <savvasiry@gmail.com>
2023-12-27 22:17:01 +1100

    feature(Loyalty): add demo block Loyalty

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -53,6 +53,37 @@
       </div>
     </div>
 
+    <LineBanner
+        :text="'СКИДКА 30%'"
+    />
+
+    <div class="loyalty-block">
+
+      <div class="loyalty-block__wrapper">
+        <h2 class="text text_normal text_white">Система лояльности</h2>
+        <h3 class="text text_medium text_white">Акции и специальные условия для постоянных посетителей</h3>
+
+        <div class="loyalty-list">
+
+          <div v-for="i in 4" :key="i" class="loyalty-card">
+            <div class="text text_semi-bold">Кэшбек до 5%</div>
+            <div class="text text_medium">При участии в нашей системе лояльсности</div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="loyalty-block__back"/>
+
+      <div class="loyalty-block__img-container">
+        <img class="loyalty-block__img" src="../assets/info-block.png" alt="Info">
+      </div>
+    </div>
+
+    <LineBanner
+        :text="'СКИДКА 30%'"
+    />
+
 
   </main>
 
@@ -152,6 +183,7 @@ export default {
 .menu {
   margin-left: 20px;
   margin-right: 20px;
+  margin-bottom: 200px;
 
   text-align: center;
 
@@ -203,7 +235,95 @@ export default {
   flex-direction: column;
   align-items: center;
   gap: 12px;
+}
 
+.loyalty-block {
+  position: relative;
+
+  width: 100%;
+  height: 720px;
+
+  overflow: hidden;
+
+  @media @min990 {
+    height: 1100px;
+  }
+
+  @media @min1200 {
+    height: 920px;
+  }
+}
+
+.loyalty-block__wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  height: 100%;
+}
+
+.loyalty-list {
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 22px;
+
+  @media @min1200 {
+    grid-template-columns: 1fr 1fr;
+  }
+}
+
+.loyalty-card {
+  width: 550px;
+
+  box-sizing: border-box;
+  padding: 25px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 13px;
+  justify-content: space-between;
+
+  background: @WhiteColor;
+  border-radius: 10px;
+}
+
+.loyalty-block__img-container {
+  position: absolute;
+  z-index: -2;
+  width: 100%;
+  height: 100%;
+
+  top: 0;
+  left: 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+}
+
+.loyalty-block__img {
+  height: 100%;
+  width: auto;
+
+  @media @min1400 {
+    height: auto;
+    width: 100%;
+  }
+}
+
+.loyalty-block__back {
+  position: absolute;
+  z-index: -1;
+
+  top: 0;
+  left: 0;
+
+  width: 100%;
+  height: 100%;
+
+  background: rgba(0, 0, 0, 0.5);
 }
 
 </style>
