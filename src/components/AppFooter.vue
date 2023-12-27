b4295073d576af63c51ec2fab91501d84432c336
Savva Siry <savvasiry@gmail.com>
2023-12-27 22:28:51 +1100

    feature(Footer): add Footer demo

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,8 +1,15 @@
 <template>
   <footer>
     <div class="footer">
-
+      <img class="footer__img" src="../assets/logo.png" alt="">
+      <div class="row row_gap40">
+        <div class="text text_bold text_white">Главная</div>
+        <div class="text text_bold text_white">Блюда</div>
+        <div class="text text_bold text_white">Система лояльности</div>
+        <div class="text text_bold text_white">Адреса</div>
+      </div>
     </div>
+
   </footer>
 
 </template>
@@ -17,8 +24,13 @@ export default {
 @import "../style.less";
 
 .footer {
+  position: relative;
   width: 100%;
   height: 490px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
   background: @BlackColor;
 
@@ -29,6 +41,11 @@ export default {
   @media @min990 {
     height: 272px;
   }
+}
+
+.footer__img {
+  position: absolute;
+  left: 120px;
 }
 
 </style>
