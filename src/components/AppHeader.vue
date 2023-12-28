d8ae4eb592b0db242ca4a10dbe9ce6804847d5dd
Savva Siry <savvasiry@gmail.com>
2023-12-29 07:34:16 +1100

    fix(header): fix paddings in header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,27 +1,24 @@
 <template>
   <header>
 
-    <div class="wrapper wrapper_paddings">
 
-      <div class="header">
+    <div class="header">
 
-        <div class="header__container">
-          <img class="header__logo" src="../assets/logo.png" alt="Logo">
-          <div v-if="isWidth580()" class="text header__nav text_bold">
-            <div class="header__nav__item">Меню</div>
-            <div class="header__nav__item">Акции</div>
-            <div class="header__nav__item">Как нас найти</div>
-          </div>
-          <div class="text header__phone text_bold">+ 7 (921) 483 38 83</div>
-        </div>
-
-        <div v-if="!isWidth580()" class="text header__nav text_bold">
+      <div class="header__container">
+        <img class="header__logo" src="../assets/logo.png" alt="Logo">
+        <div v-if="isWidth580()" class="text header__nav text_bold">
           <div class="header__nav__item">Меню</div>
           <div class="header__nav__item">Акции</div>
           <div class="header__nav__item">Как нас найти</div>
         </div>
+        <div class="text header__phone text_bold">+ 7 (921) 483 38 83</div>
       </div>
 
+      <div v-if="!isWidth580()" class="text header__nav text_bold">
+        <div class="header__nav__item">Меню</div>
+        <div class="header__nav__item">Акции</div>
+        <div class="header__nav__item">Как нас найти</div>
+      </div>
     </div>
 
   </header>
@@ -40,11 +37,15 @@ export default {
 @import "../style";
 
 .header {
-  width: 100%;
+  width: auto;
   height: 138px;
 
   box-sizing: border-box;
-  padding: 16px 0;
+  padding: 16px 20px;
+
+  @media @min1200 {
+    margin: 0 100px;
+  }
 
   display: flex;
   flex-direction: column;
