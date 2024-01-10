1e752cab8c7672721982754fa2dec5841637c25f
Savva Siry <savvasiry@gmail.com>
2024-01-10 17:35:52 +1100

    core(Env): add key to header

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@ import {createYmaps} from "vue-yandex-maps";
 const app = createApp(App);
 
 app.use(createYmaps({
-    apikey: import.meta.env.VITE_Y_API_KEY,
+    // apikey: import.meta.env.VITE_Y_API_KEY,
+    apikey: 'cb9e76f3-8052-4d2c-8fc8-4cd3b4a8c405',
 }));
 
 app.mount('#app');
